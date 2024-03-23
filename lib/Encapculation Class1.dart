41f9d3a6d2ffc17b8c8667c707ad810aca5cc885
DevNadimm <nadimchowdhury87@gmail.com>
2024-03-23 13:17:15 +0600

    Code Updated get and set

--- lib/Encapculation Class1.dart
+++ lib/Encapculation Class1.dart
@@ -2,32 +2,43 @@ class Person {
   String _firstName;
   String _lastName;
   String _gender;
-  int age;
+  int _age;
 
-  Person(this._firstName, this._lastName, this._gender, this.age);
+  Person(this._firstName, this._lastName, this._gender, this._age);
 
   //Getter method
-  getFullName(){
-    String fullName = _firstName + ' ' + _lastName;
-    return(fullName);
+  get getFullName{
+    return _firstName + ' ' + _lastName;
   }
 
   // Setter method
-  changeGendar (String newGender){
+  set changeGendar (String newGender){
     if (newGender == 'Male' || newGender == 'Female'){
       _gender = newGender;
     }
   }
 
   //getter method
-  getGender(){
+  String get getGender{
     return _gender;
   }
 
   //getter method
-  getBirthYear (){
-    int birthYear = 2024 - age;
+  int get getBirthYear{
+    int birthYear = 2024 - _age;
     return(birthYear);
+  }
+
+  //setter method
+  set changeAge(int newAge) {
+    if (newAge > 12 && newAge < 100) {
+      _age = newAge;
+    }
+  }
+
+  //get
+  get getAge {
+    return _age;
   }
 
 }
--- lib/Encapculation_live2.dart
+++ lib/Encapculation_live2.dart
@@ -3,11 +3,14 @@ import 'Encapculation Class1.dart';
 void main (){
   Person defaultPerson = Person('Rafiqul', 'Islam', 'Male', 20);
 
-  print(defaultPerson.getFullName());
-  print(defaultPerson.getGender());
+  print(defaultPerson.getFullName);
+  print(defaultPerson.getGender);
 
-  defaultPerson.changeGendar('Female');
-  print(defaultPerson.getGender());
+  defaultPerson.changeGendar = 'Female';
+  print(defaultPerson.getGender);
 
-  print(defaultPerson.getBirthYear());
+  print(defaultPerson.getBirthYear);
+
+  defaultPerson.changeAge = 89;
+  print(defaultPerson.getAge);
 }
