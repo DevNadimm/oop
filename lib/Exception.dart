6d047b3908944e5f45aae753077abc394a7f5779
DevNadimm <nadimchowdhury87@gmail.com>
2024-03-26 21:57:45 +0600

    Code Updated

---
+++ lib/Exception.dart
@@ -0,0 +1,32 @@
+//Try, On
+
+//void main() {
+//   try {
+//     var division = 10 ~/ 0;
+//     print(division);
+//   } catch (e){
+//     print(e);
+//   }
+// }
+
+
+// Try, On, Catch
+
+// void main() {
+//   try {
+//     var division = 10 ~/ 0;
+//     print(division);
+//   } on IntegerDivisionByZeroException{
+//     print('Enter a amount which is greater than 0');
+//   } catch (e){
+//     print(e);
+//   }
+// }
+
+
+// Custom Exception
+
+
+
+
+
---
+++ lib/switch case.dart
@@ -0,0 +1,25 @@
+// void main() {
+//   var age = 18;
+//   switch (age) {
+//     case 18:
+//       print("বয়স 18");
+//     case 22:
+//       print("বয়স 22");
+//     default:
+//       print("ডিফল্ট");
+//   }
+// }
+
+void main() {
+  var age = 18;
+  switch (age) {
+    case 18:
+    case 22:
+      print("বয়স 22");
+    case 23:
+      print("বয়স 23");
+    default:
+      print("ডিফল্ট");
+  }
+}
+
