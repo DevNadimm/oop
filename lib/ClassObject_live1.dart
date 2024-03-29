c9bfb68c935e281c0ac0635568eb35f83e15dd56
Nadim Chowdhury <161572904+DevNadimm@users.noreply.github.com>
2024-03-29 14:40:05 +0600

    Update ClassObject_live1.dart

--- lib/ClassObject_live1.dart
+++ lib/ClassObject_live1.dart
@@ -27,7 +27,7 @@ class Student {
   int age = 0;                  // String name <= properties / variable / field
   String address = '';
   static String institution = 'Dhaka University';  //non changeable propertie     er mane hocce object etake change korte parbe na but class change korte parbe
-  //static final String institution = 'Dhaka University';  //non changeable propertie but ekhane object thekeo change korte parbe na
+  //static final String institution = 'Dhaka University';  //non changeable propertie but ekhane class thekeo change korte parbe na
 
   void helloNadim (){
     print('Hello Nadim');       // helloNadim () <= Methode
@@ -42,7 +42,7 @@ class Teacher {
   Teacher({required this.name, required this.age, required this.department});
 }
 
-
+//============================================================================================================================
 
 // void main(){
 //   List <Teacher> teachers = [];
@@ -65,4 +65,31 @@ class Teacher {
 //   String subject ;
 //   String department ;
 //   Teacher({required this.name, required this.subject, required this.department});
+// }
+
+//============================================================================================================================
+
+// class myClass{
+//   int a=10;
+//   int b=11;
+//   List<String> studentName = ['Nadim','Rafat','Rahim'];
+//   List<Map<String, dynamic>> groceryList = [
+//     {'name': 'Apple', 'price': 1.99},
+//     {'name': 'Banana', 'price': 0.79},
+//     {'name': 'Milk', 'price': 2.49},
+//     {'name': 'Bread', 'price': 3.99},
+//     {'name': 'Eggs', 'price': 2.29},
+//   ];
+// }
+
+// void main (){
+//   var classObj = myClass();  //Object Create
+//   int towSum =(classObj.a+classObj.b);
+//   print(towSum);
+//   print(classObj.studentName);
+//   for(int i = 0; i<classObj.groceryList.length; i++) {
+//     String name = classObj.groceryList[i]['name'];
+//     double price = classObj.groceryList[i]['price'];
+//     print("Product name is $name and price is $price Doller");
+//   }
 // }
